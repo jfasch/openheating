c56f55541573ac64738e966144dca03f89f04f9d
Joerg Faschingbauer <jf@faschingbauer.co.at>
2014-03-14 20:57:59 +0100

    transport: anti_oscillating_threshold

--- heating/control/transport.py
+++ heating/control/transport.py
@@ -1,14 +1,37 @@
 class Transport:
-    def __init__(self, producer, consumer, pump):
+    def __init__(self, producer, consumer, anti_oscillating_threshold, pump):
         self.__producer = producer
         self.__consumer = consumer
+        self.__anti_oscillating_threshold = anti_oscillating_threshold
         self.__pump = pump
 
+        # had we reached the consumer's wanted_temperature already
+        # before? if so, we don't switch on the pump again unless we
+        # see a consumer temperature below a restart_delay threshold
+        # diff.
+        self.__falling = False
+
     def move(self):
+        # off if consumer's wanted_temperature reached.
         if self.__consumer.temperature() >= self.__consumer.wanted_temperature():
             if self.__pump.is_running():
                 self.__pump.stop()
-        else:
-            if not self.__pump.is_running():
-                self.__pump.start()
- 
+            self.__falling = True
+            return
+
+        # don't care if pump is on; leave it running.
+        if self.__pump.is_running():
+            return
+
+        # consumer's temperature below wanted. are we falling? if so,
+        # then we better wait until the temperature is below our
+        # anti-oscillating threshold
+
+        if not self.__falling:
+            self.__falling = False
+            self.__pump.start()
+            return
+
+        if self.__consumer.temperature() < self.__consumer.wanted_temperature() - self.__anti_oscillating_threshold:
+            self.__pump.start()
+            return
--- heating/tests/transport_tests.py
+++ heating/tests/transport_tests.py
@@ -13,6 +13,8 @@ class TestConsumer(Consumer):
         return self.__temperature
     def wanted_temperature(self):
         return self.__wanted_temperature
+    def set_temperature(self, temperature):
+        self.__temperature = temperature
 
 class TestProducer(Producer):
     def __init__(self, temperature):
@@ -31,16 +33,44 @@ class TestPump(Pump):
         self.__is_running = False
 
 class TransportTest(unittest.TestCase):
-    def test_basic(self):
+    def test_pump_on_off_simple(self):
         consumer = TestConsumer(wanted_temperature=40, initial_temperature=20)
         producer = TestProducer(temperature=80)
         pump = TestPump()
-        transport = Transport(producer=producer, consumer=consumer, pump=pump)
+        transport = Transport(producer=producer, consumer=consumer, anti_oscillating_threshold=7, pump=pump)
 
+        # pump is off initially. switched on after first move, due to
+        # difference of 40 degrees.
+        self.failIf(pump.is_running())
         transport.move()
-
         self.failUnless(pump.is_running())
 
+        # consumer reaches temperature, pump switched off
+        consumer.set_temperature(40)
+        transport.move()
+        self.failIf(pump.is_running())
+
+        # consumer's temperature falls by a lot of degrees (20 is a
+        # lot), pump switched on again.
+        consumer.set_temperature(20)        
+        transport.move()
+        self.failUnless(pump.is_running())
+
+    def test_restart_delay(self):
+        # consumer is satisfied with its temperature, pump not running
+        # initially. consumer's temperature falls, say, 1 degree below
+        # wanted, pump *not* switched on immediately.
+        consumer = TestConsumer(wanted_temperature=40, initial_temperature=40)
+        producer = TestProducer(temperature=80)
+        pump = TestPump()
+        transport = Transport(producer=producer, consumer=consumer, anti_oscillating_threshold=7, pump=pump)
+
+        transport.move()
+        self.failIf(pump.is_running())
+
+        consumer.set_temperature(39)
+        transport.move()
+        self.failIf(pump.is_running())
 
 suite = unittest.TestSuite()
 suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TransportTest))
