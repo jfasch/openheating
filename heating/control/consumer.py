9827f469c691a2eb05c50c82c8a90712b9073c01
Joerg Faschingbauer <jf@faschingbauer.co.at>
2014-03-26 11:51:41 +0100

    Consumer, Producer: ABC

--- heating/control/consumer.py
+++ heating/control/consumer.py
@@ -1,2 +1,12 @@
-class Consumer:
-    pass
+from abc import ABCMeta, abstractmethod
+
+class Consumer(metaclass=ABCMeta):
+    @abstractmethod
+    def temperature(self):
+        assert False, 'abstract'
+        return 24.3
+    @abstractmethod
+    def wanted_temperature(self):
+        assert False, 'abstract'
+        return 55.9
+
--- heating/control/producer.py
+++ heating/control/producer.py
@@ -1,2 +1,7 @@
-class Producer:
+from abc import ABCMeta, abstractmethod
+
+class Producer(metaclass=ABCMeta):
+    def temperature(self):
+        assert False, 'abstract'
+        return 25.4
     pass
