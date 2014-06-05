c9baece7856b447724148e80b6c7b15e931e3d67
Joerg Faschingbauer <jf@faschingbauer.co.at>
2014-06-05 12:38:10 +0000

    i2c hwmon thermometers

--- heating/hardware/thermometer_hwmon.py
+++ heating/hardware/thermometer_hwmon.py
@@ -1,6 +1,10 @@
 from ..control.thermometer import Thermometer
+from ..control.error import HeatingException
 
 import os
+
+_HWMON_CLASS = '/sys/class/hwmon'
+_TEMP_INPUT = 'temp1_input'
 
 class HWMON_Thermometer(Thermometer):
     def __init__(self, number):
@@ -13,6 +17,23 @@ class HWMON_Thermometer(Thermometer):
         temp = open(self.__temp_input, 'r').read()
         return int(temp)/1000
 
+class HWMON_I2C_Thermometer(HWMON_Thermometer):
+    def __init__(self, bus_number, address):
+        device_dir = '/sys/bus/i2c/devices/i2c-%d/%d-%04x' % (bus_number, bus_number, address)
+        if not os.path.isdir(device_dir):
+            raise HeatingException('HWMON I2C Thermometer: no such device: '+device_dir)
+        device_hwmon_dir = os.path.join(device_dir, 'hwmon')
+        if not os.path.isdir(device_dir):
+            raise HeatingException('HWMON I2C Thermometer: not a hwmon device; missing '+device_hwmon_dir)
+        hwmon_devices = os.listdir(device_hwmon_dir)
+        if len(hwmon_devices) == 0:
+            raise HeatingException('HWMON I2C Thermometer: no device found in '+device_hwmon_dir)
+        if len(hwmon_devices) != 1:
+            raise HeatingException('HWMON I2C Thermometer: not implemented: found multiple (%d) devices in %s' % \
+                                       (len(hwmon_devices), device_hwmon_dir))
+        number = int(hwmon_devices[0][5:])
+        HWMON_Thermometer.__init__(self, number)
+
 def iter_devices():
     for d in os.listdir(_HWMON_CLASS):
         if not d.startswith('hwmon'):
@@ -22,9 +43,6 @@ def iter_devices():
             # obviously not a thermometer; skip
             continue
         yield number, HWMON_Thermometer(number)
-
-_HWMON_CLASS = '/sys/class/hwmon'
-_TEMP_INPUT = 'temp1_input'
 
 def _input_file(number):
     return os.path.join(_HWMON_CLASS, 'hwmon%d' % number, _TEMP_INPUT)
