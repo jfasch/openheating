90a9a4af3dd91dc4eb73dfda8aecaa520e1587e1
Joerg Faschingbauer <jf@faschingbauer.co.at>
2014-10-24 22:02:34 +0200

    HeatingException: support nested errors

--- heating/error.py
+++ heating/error.py
@@ -6,7 +6,8 @@ class HeatingException(Exception):
     ImportError and whatnot, and our own errors.
     '''
 
-    def __init__(self, msg):
+    def __init__(self, msg, nested_errors=None):
         Exception.__init__(self, msg)
+        self.__nested_errors = nested_errors
         pass
     pass
