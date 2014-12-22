84b8a75d8edfd60b9e5f9b6407dcce1bbae5dff1
Joerg Faschingbauer <jf@faschingbauer.co.at>
2014-12-22 09:42:55 +0000

    service startup: provide common base class

--- bin/openheating-dbus-thermometer-service.py
+++ bin/openheating-dbus-thermometer-service.py
@@ -2,65 +2,34 @@
 
 from openheating.thermometer_dbus_object import DBusThermometerObject
 from openheating.thermometer_dbus_config import ThermometerDBusServiceConfigParser
+from openheating.dbus_util import DBusServiceCombo
 
-import dbus
-from dbus.mainloop.glib import DBusGMainLoop
-from gi.repository import GLib
-import logging
 from argparse import ArgumentParser
-import sys
-import os
-import time
-import signal
+import logging
+import logging.handlers
+
 
 parser = ArgumentParser()
 parser.add_argument('--config-file', type=str, help='Configuration file (to be documented)', required=True)
 parser.add_argument('--pid-file', type=str, help='PID file (to be documented)')
+parser.add_argument('--syslog', action='store_true', default=False, help='PID file (to be documented)')
 args = parser.parse_args()
+
+if args.syslog:
+    h = logging.handlers.SysLogHandler(address='/dev/log')
+    logging.getLogger().addHandler(h)
 
 config = ThermometerDBusServiceConfigParser().parse(open(args.config_file).read())
 
-childpid = None
-parentpid = os.getpid()
-
-def terminate_handler(signal, frame):
-    # kill child. the terminate-signal could well arrive *after*
-    # the child has exited and *before* it is restarted, so its
-    # pid might not be valid - so we have to ignore errors.
-    if childpid is not None:
-        try:
-            os.kill(childpid, signal)
-        except OSError: pass
-    # remove pidfile
-    if args.pid_file is not None:
-        os.remove(args.pid_file)
-    sys.exit(0)
-signal.signal(signal.SIGTERM, terminate_handler)
-
-if args.pid_file is not None:
-    open(args.pid_file, 'w').write(str(parentpid)+'\n')
-
-while True:
-    childpid = os.fork()
-    if childpid > 0:
-        # parent. wait for child, restart and backoff
-        died, status = os.wait()
-        time.sleep(2)
-    else:
-        mainloop = DBusGMainLoop(set_as_default=True)
-
-        try:
-            connection = dbus.bus.BusConnection(config.daemon_address(), mainloop=mainloop)
-        except dbus.exceptions.DBusException as e:
-            logging.exception('cannot connect to '+config.daemon_address())
-            sys.exit(1)
- 
-        connection.set_exit_on_disconnect(True)
-        busname = dbus.service.BusName(config.bus_name(), connection)
-
-        for t in config.thermometers():
+class MyCombo(DBusServiceCombo):
+    def __init__(self, pidfile, config):
+        DBusServiceCombo.__init__(self, pidfile, config.daemon_address(), config.bus_name())
+        self.__thermometers = config.thermometers()
+    def create_objects(self, connection):
+        for t in self.__thermometers:
             ctor_params = { 'connection': connection }
             ctor_params.update(t)
             DBusThermometerObject(**ctor_params)
 
-        GLib.MainLoop().run()
+combo = MyCombo(pidfile=args.pid_file, config=config)
+combo.run()
--- openheating/dbus_util.py
+++ openheating/dbus_util.py
@@ -1,1 +1,84 @@
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
+
+from abc import ABCMeta, abstractmethod
+import logging
+import sys
+import signal
+import os
+import time
+
 DBUS_THERMOMETER_IFACE_STRING = 'org.openheating.Thermometer'
+
+class DBusServiceCombo:
+    def __init__(self, pidfile, daemon_address, busname):
+        self.__pidfile = pidfile
+        self.__daemon_address = daemon_address
+        self.__busname = busname
+        self.__child = None
+
+    def run(self):
+        # there must be only one per process
+        global _object
+        assert _object is None
+
+        _object = self
+
+        signal.signal(signal.SIGTERM, _terminate_handler)
+        signal.signal(signal.SIGQUIT, _terminate_handler)
+        signal.signal(signal.SIGINT, _terminate_handler)
+
+        if self.__pidfile is not None:
+            open(self.__pidfile, 'w').write(str(os.getpid())+'\n')
+
+        while True:
+            self.__child = os.fork()
+            if self.__child > 0:
+                # parent. wait for child, and backoff before restart
+                died, status = os.wait()
+                logging.warning('child %d died, status %d' % (died, status))
+                time.sleep(2)
+            else:
+                signal.signal(signal.SIGTERM, signal.SIG_DFL)
+                signal.signal(signal.SIGQUIT, signal.SIG_DFL)
+                signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+                mainloop = DBusGMainLoop(set_as_default=True)
+        
+                try:
+                    connection = dbus.bus.BusConnection(self.__daemon_address, mainloop=mainloop)
+                except dbus.exceptions.DBusException as e:
+                    logging.exception('cannot connect to '+self.__daemon_address)
+                    sys.exit(1)
+         
+                connection.set_exit_on_disconnect(True)
+                busname = dbus.service.BusName(self.__busname, connection)
+
+                # tell derived class to fill in what's needed
+                self.create_objects(connection)
+        
+                GLib.MainLoop().run()
+
+    @abstractmethod
+    def create_objects(self, connection):
+        pass
+
+    def terminate(self, signum):
+        # kill child. the terminate-signal could well arrive *after*
+        # the child has exited and *before* it is restarted, so its
+        # pid might not be valid - so we have to ignore errors.
+        if self.__child is not None:
+            try:
+                os.kill(self.__child, signum)
+            except OSError: pass
+        # remove pidfile
+        if self.__pidfile is not None:
+            os.remove(self.__pidfile)
+        sys.exit(0)
+
+_object = None
+
+def _terminate_handler(signum, frame):
+    if _object is not None:
+        _object.terminate(signum)
