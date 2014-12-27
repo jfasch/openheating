4240abe9a7928c28126813388a70df1c0c0d2d07
Joerg Faschingbauer <jf@faschingbauer.co.at>
2014-12-28 00:22:23 +0100

    lcd-proto.py: add datetime.now()

--- bin/lcd-proto.py
+++ bin/lcd-proto.py
@@ -5,6 +5,7 @@ from openheating.dbus.thermometer import DBusThermometer
 
 import dbus.bus
 import time
+from datetime import datetime
 
 connection = dbus.bus.BusConnection('tcp:host=192.168.1.11,port=6666')
 display = HD44780_LCD(
@@ -28,6 +29,7 @@ ofen = DBusThermometer(connection=connection, name='org.openheating.ofen', path=
 
 while True:
     temps = {
+        'now': str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
         'boiler-top': boiler_top.temperature(),
         'boiler-middle': boiler_middle.temperature(),
         'boiler-bottom': boiler_bottom.temperature(),
@@ -37,9 +39,10 @@ while True:
         'ofen': ofen.temperature(),
         }
     msg = \
-        ('Boi:%(boiler-top).1f/%(boiler-middle).1f/%(boiler-bottom).1f\n' + \
-        'HK:%(hk-vl).1f,WW:%(boiler-vl).1f\n' + \
-        'Ofen:%(ofen).1f,VL:%(ofen-vl).1f') % temps
+        ('%(now)s\n' + \
+         'Boi:%(boiler-top).1f/%(boiler-middle).1f/%(boiler-bottom).1f\n' + \
+         'HK:%(hk-vl).1f,WW:%(boiler-vl).1f\n' + \
+         'Ofen:%(ofen).1f,VL:%(ofen-vl).1f') % temps
 
     print(msg+'\n--')
 
