604754bcb82435e59059f685e2bdf02e026fe41f
Joerg Faschingbauer <jf@faschingbauer.co.at>
2015-01-02 20:45:23 +0000

    dbus: implement DBusThermometer in terms of rebind.DBusObjectClient

--- openheating/dbus/thermometer_client.py
+++ openheating/dbus/thermometer_client.py
@@ -1,28 +1,8 @@
-from .types import exception_dbus_to_local
+from .rebind import DBusObjectClient
 
 from ..thermometer import Thermometer
-from ..error import HeatingError, DBusNoConnectionError, DBusNoServiceError
-
-from dbus.exceptions import DBusException
 
 
-class DBusThermometer(Thermometer):
-    def __init__(self, connection_proxy, name, path):
-        self.__connection_proxy = connection_proxy
-        self.__name = name
-        self.__path = path
-        self.__object = None
+class DBusThermometer(Thermometer, DBusObjectClient):
     def temperature(self):
-        try:
-            if self.__object is None:
-                self.__object = self.__connection_proxy.get_connection().get_object(self.__name, self.__path)
-            return float(self.__object.temperature())
-        except DBusException as e:
-            if e.get_dbus_name() in ('org.freedesktop.DBus.Error.Disconnected', 'org.freedesktop.DBus.Error.NoServer'):
-                self.__connection_proxy.clear_connection()
-                self.__object = None
-                raise DBusNoConnectionError(msg=str(e))
-            if e.get_dbus_name() in ('org.freedesktop.DBus.Error.ServiceUnknown'):
-                self.__object = None
-                raise DBusNoServiceError(msg=str(e))
-            raise exception_dbus_to_local(e)
+        return float(self.dbus_call('temperature'))
