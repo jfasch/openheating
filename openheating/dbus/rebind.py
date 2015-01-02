5f380042022454b1820319da835d6e14903c0e45
Joerg Faschingbauer <jf@faschingbauer.co.at>
2015-01-02 21:51:35 +0100

    dbus: rebind: docstrings

--- openheating/dbus/rebind.py
+++ openheating/dbus/rebind.py
@@ -1,3 +1,17 @@
+'''"rebinding" wrappers of both DBus connections and DBus objects.
+
+DBus connections and objects tend to break on connection loss on
+either side. If a client connection to the bus breaks, all is lost. If
+an object's reference to a service/object breaks due to it's loss of
+connection to the bus, then the local object proxy is dead.
+
+openheating's use of objects is through instances of DBusObjectClient
+(one is supposed to derive from it), and the overall
+DBusConnectionProxy which is instantiated once and passed in. Recovery
+is done inside, on both connections and objects.
+
+'''
+
 from .types import exception_dbus_to_local
 from ..error import HeatingError, DBusNoConnectionError, DBusNoServiceError
 
@@ -5,6 +19,13 @@ from dbus.exceptions import DBusException
 import dbus.bus
 
 class DBusConnectionProxy:
+    '''Recovery of DBus connection
+
+    User code instantiates one and passes it to DBusObjectClient
+    instances which do the rest.
+
+    '''
+
     def __init__(self, address):
         self.__address = address
         self.__connection = None
@@ -18,6 +39,15 @@ class DBusConnectionProxy:
         self.__connection = None
 
 class DBusObjectClient:
+    '''Recovery of DBus object proxy
+
+    User code is supposed to derive and implement its methods using
+    base's dbus_call(), with the name of the method and positional
+    parameters. (Keyword parameters are not supported, as this is not
+    the nature of the DBus protocol.
+
+    '''
+    
     def __init__(self, connection_proxy, name, path):
         self.__connection_proxy = connection_proxy
         self.__name = name
