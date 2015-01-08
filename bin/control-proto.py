ad8436417606b8a64aec871846b96ec4ccc01ba3
Joerg Faschingbauer <jf@faschingbauer.co.at>
2015-01-08 18:03:07 +0100

    control-proto: couple hacks

--- bin/control-proto.py
+++ bin/control-proto.py
@@ -13,25 +13,45 @@ from openheating.hysteresis import Hysteresis
 from openheating.thinking import Brain
 from openheating.error import HeatingError
 
+import logging
 import sys
 import time
 from datetime import datetime
+
+logging.basicConfig(level=logging.DEBUG)
 
 connection_proxy = DBusConnectionProxy('tcp:host=192.168.1.11,port=6666')
 thermo_center = DBusThermometerCenter(connection_proxy=connection_proxy, name='org.openheating.thermometer_center', path='/thermometer_center')
 switch_center = DBusSwitchCenter(connection_proxy=connection_proxy, name='org.openheating.switch_center', path='/switch_center')
 
+th_boiler_top = thermo_center.get_thermometer('boiler-top')
+th_ofen = thermo_center.get_thermometer('ofen')
+th_oil =  thermo_center.get_thermometer('oel-puffer')
+
+# sw_pumpe_ww = TestSwitch(name="pumpe-ww", initial_state=Switch.OPEN, output=sys.stdout)
+# sw_oil_enable = TestSwitch(name="enable", initial_state=Switch.OPEN, output=sys.stdout)
+# sw_oil_burn = TestSwitch(name="burn", initial_state=Switch.OPEN, output=sys.stdout)
+
+sw_pumpe_ww = switch_center.get_switch('pumpe-ww')
+sw_oil_enable = switch_center.get_switch('oel-enable')
+sw_oil_burn = switch_center.get_switch('oel-burn')
+
+
 sink = Sink(
     name='boiler', 
-    thermometer=thermo_center.get_thermometer('boiler-top'), 
-    hysteresis=Hysteresis(low=50, high=55))
+    thermometer=th_boiler_top, 
+    hysteresis=Hysteresis(low=50, high=55),
+#    hysteresis=Hysteresis(low=20, high=30),
+)
 
 # source = OilCombo(
 #     name='oil',
-#     enable_switch=TestSwitch(name="enable", initial_state=Switch.OPEN, output=sys.stdout),
-#     burn_switch=TestSwitch(name="burn", initial_state=Switch.OPEN, output=sys.stdout),
-#     thermometer=thermo_center.get_thermometer('oel-puffer'))
-source = PassiveSource(name='ofen', thermometer=thermo_center.get_thermometer('ofen'))
+#     enable_switch=sw_oil_enable,
+#     burn_switch=sw_oil_burn,
+#     thermometer=th_oil)
+# source.enable()
+
+source = PassiveSource(name='ofen', thermometer=th_ofen)
 
 transport = Transport(
     name='ww',
@@ -39,14 +59,12 @@ transport = Transport(
     sink=sink, 
     # adapt hysteresis to something more realistic
     diff_hysteresis=Hysteresis(low=0, high=1), 
-    pump_switch=TestSwitch(name="pumpe-hk", initial_state=Switch.OPEN, output=sys.stdout))
+    pump_switch=sw_pumpe_ww)
 
 brain = Brain()
 brain.add(sink)
 brain.add(source)
 brain.add(transport)
-
-#source.enable()
 
 round = 0
 while True:
