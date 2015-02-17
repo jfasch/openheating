542881f4da040fae54e97e8df88b861e89ed98a1
Joerg Faschingbauer <jf@faschingbauer.co.at>
2015-02-17 11:26:21 +0100

    thinking: formalize ThinkingSwitch a bit more

--- openheating/oil.py
+++ openheating/oil.py
@@ -31,32 +31,24 @@ class OilCombo(Source, Thinker):
                  minimum_temperature_hysteresis):
         Source.__init__(self, name=name)
         
-        self.__burn_switch = burn_switch
+        self.__burn_switch = ThinkingSwitch(burn_switch)
         self.__thermometer = thermometer
         self.__minimum_temperature_hysteresis = minimum_temperature_hysteresis
-
-        # used during a thinking round. initialized at first think(),
-        # reaped (and reset) at sync().
-        self.__think_switch = None
 
     def temperature(self):
         return self.__thermometer.temperature()
 
     def think(self):
-        # first thinking round; set up the "thinking switch" logic
-        if self.__think_switch is None:
-            self.__think_switch = ThinkingSwitch(self.__burn_switch)
-
         temperature = self.__thermometer.temperature()
 
         if self.num_requesters() > 0 or self.__minimum_temperature_hysteresis.below(temperature):
-            return self.__think_switch.set(True)
+            return self.__burn_switch.set(True)
 
         if self.num_requesters() == 0 and self.__minimum_temperature_hysteresis.above(temperature):
-            return self.__think_switch.set(False)
+            return self.__burn_switch.set(False)
 
         return 0
         
     def sync(self):
-        self.__think_switch.sync()
-        self.__think_switch = None
+        self.__burn_switch.sync()
+        self.__burn_switch.reset()
--- openheating/thinking.py
+++ openheating/thinking.py
@@ -39,18 +39,32 @@ class Brain:
         self.__round += 1
 
 class ThinkingSwitch:
-    '''Operates on a switch. Returns >0 everytime a "set" operation
-    modifies the current switch state, and 0 otherwise. 
+    '''A bridge that makes a switch think a bit
 
-    This reflects "thinking" because a "thought" is made on
-    modification, and none otherwise.
+    Sometimes a "Thinker" makes decisions based on temperatures, and
+    sets a switch accordingly (for example, a pump is switched on
+    because source and sink temperatures differ sufficiently). Setting
+    the switch not necessarily changes its state - the switch might
+    have had that state before. Only when the state changes, this
+    weighs in as a "thought".
+
+    Consequentially a ThinkingSwitch encapsulates (has) a switch, and
+    operates on it accordingly in the set() method.
+
     '''
 
     def __init__(self, switch):
-        self.__state = None
         self.__switch = switch
+        self.__state = None
 
     def set(self, state):
+        '''Set the switch to state (a bool value)
+        
+        Returns > 0 if the switch state had to change (a
+        decision/thought was made), and 0 otherwise.
+
+        '''
+
         assert type(state) is bool
         
         if state != self.__state:
@@ -62,3 +76,6 @@ class ThinkingSwitch:
     def sync(self):
         if self.__state is not None:
             self.__switch.set_state(self.__state)
+
+    def reset(self):
+        self.__state = None
