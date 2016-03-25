3a3041f9133ca4b58689a5e3c52c4844d3d642f2
Joerg Faschingbauer <jf@faschingbauer.co.at>
2016-03-25 15:14:35 +0100

    sink: remove transport

--- openheating/logic/sink.py
+++ openheating/logic/sink.py
@@ -11,76 +11,67 @@ class Sink(Thinker):
         Thinker.__init__(self, name)
 
         self.__thermometer = thermometer
+        self.__pump_switch = pump_switch
         self.__temperature_range = temperature_range
+        self.__diff_hysteresis = diff_hysteresis
         self.__source = None
 
-        # parameters for our transport. that will be made once we know
-        # the source we are attached to.
-        self.__diff_hysteresis = diff_hysteresis
-        self.__pump_switch = pump_switch
-
-        self.__transport = None
-
-    # jjj get rid of this once we got rid of transport
-    def temperature(self):
-        return self.__current_temperature
-
-    # jjj remove. no sink needs to know its source.
     def set_source(self, source):
         assert self.__source is None
-
         self.__source = source
 
-        # jjj create transport. we don't need the parameters anymore,
-        # so del them afterwards.
-        if True:
-            self.__transport = Transport(name=self.__source.name()+'->'+self.name(),
-                                         source=self.__source,
-                                         sink=self,
-                                         diff_hysteresis=self.__diff_hysteresis, 
-                                         pump_switch=self.__pump_switch)
-            del self.__diff_hysteresis
-            del self.__pump_switch
-
     def init_thinking_local(self):
-        self.__transport.init_thinking_local()
-
-        # jjj this need not be a member - it's just that Transport
-        # needs it in its decisions. get rid of it once we got rid of
-        # transport.
-        self.__current_temperature = self.__thermometer.temperature()
-
-        if self.__temperature_range.below(self.__current_temperature):
-            # temperature below range, definitely in need for heat
-            msg = '%.1f below %s -> need' % (self.__current_temperature, str(self.__temperature_range))
-            self.__debug(msg)
-            self.__need = True
-        elif self.__temperature_range.above(self.__current_temperature):
-            # temperature above range, but could still take.
-            msg = '%.1f above %s -> don\'t care' % (self.__current_temperature, str(self.__temperature_range))
-            self.__debug(msg)
-            self.__need = None
-        else:
-            # temperature within range, could take heat but don't
-            # need.
-            msg = '%.1f within %s -> need' % (self.__current_temperature, str(self.__temperature_range))
-            self.__debug(msg)
-            self.__need = True
+        self.__think__sink_temperature = self.__thermometer.temperature()
+        self.__think__need = None
 
     def init_thinking_global(self):
-        self.__transport.init_thinking_global()
+        self.__think__source_temperature = self.__source.temperature()
 
     def think(self):
-        return self.__transport.think()
+        thoughts = []
 
-    def finish_thinking_global(self):
-        self.__transport.finish_thinking_global()
+        # see if we need temperature. my source is then able to react
+        # accordingly, and possibly produce heat.
+        if self.__temperature_range.below(self.__think__sink_temperature):
+            # temperature below range, definitely in need for heat
+            if self.__think__need is not True:
+                msg = '%.1f below %s -> need' % (self.__think__sink_temperature, str(self.__temperature_range))
+                self.__debug(msg)
+                self.__think__need = True
+                thoughts.append((self.name(), msg))
+        elif self.__temperature_range.above(self.__think__sink_temperature):
+            # temperature above range, but could still take.
+            if self.__think__need is not False:
+                msg = '%.1f above %s -> don\'t need' % (self.__think__sink_temperature, str(self.__temperature_range))
+                self.__debug(msg)
+                self.__think__need = False
+                thoughts.append((self.name(), msg))
+
+        return thoughts
 
     def finish_thinking_local(self):
-        self.__transport.finish_thinking_local()
+        # compare source and sink temperatures, and switch on/off pump
+        # accordingly.
+        difference = self.__think__source_temperature - self.__think__sink_temperature
 
-    def test__get_need(self):
-        return self.__need
-    
+        if self.__diff_hysteresis.above(difference):
+            msg = 'pump on (source %.1f - sink %.1f) is above %s' % \
+                  (round(self.__think__source_temperature, 1), 
+                   round(self.__think__sink_temperature, 1), 
+                   str(self.__diff_hysteresis))
+            self.__debug(msg)
+            self.__pump_switch.set_state(True)
+        elif self.__diff_hysteresis.below(difference):
+            msg = 'pump off (source %.1f - sink %.1f) is below %s' % \
+                  (round(self.__think__source_temperature, 1), 
+                   round(self.__think__sink_temperature, 1), 
+                   str(self.__diff_hysteresis))
+            self.__debug(msg)
+            self.__pump_switch.set_state(False)
+
+        del self.__think__sink_temperature
+        del self.__think__source_temperature
+        del self.__think__need
+
     def __debug(self, msg):
         logger.debug('sink %s: %s' % (self.name(), msg))
