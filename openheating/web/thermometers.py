aae55905da8a08bec7e89e63fe4573db635a7bde
Joerg Faschingbauer <jf@faschingbauer.co.at>
2019-09-20 16:58:14 +0200

    web: revisit temperature histograms

--- openheating/web/thermometers.py
+++ openheating/web/thermometers.py
@@ -1,5 +1,5 @@
 from .instance import app
-from .history_utils import make_histogram_input__no_label
+from .history_utils import make_histogram_input__full_label
 
 import flask
 
@@ -10,7 +10,7 @@ import datetime
 def thermometers():
     samples_per_thermometer = {}
     for name in app.thermometers.keys():
-        samples_per_thermometer[name] = make_histogram_input__no_label(app.thermometer_histories[name].distill(
+        samples_per_thermometer[name] = make_histogram_input__full_label(app.thermometer_histories[name].distill(
             granularity=datetime.timedelta(minutes=1),
             duration=datetime.timedelta(days=1)))
 
