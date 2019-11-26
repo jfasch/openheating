d9c0c872cc90caa389c39ef559f66af21a9bbf9e
Joerg Faschingbauer <jf@faschingbauer.co.at>
2019-11-26 11:13:01 +0100

    web: home: show only four temperature charts

--- openheating/web/faschingbauer_home.py
+++ openheating/web/faschingbauer_home.py
@@ -4,34 +4,27 @@ from . import svg
 import datetime
 
 
-_thermometer_names = (
+_top_thermometer_names = (
     'Raum',
     'SpeicherOben',
     'Holzbrenner',
     'Oelbrenner',
-
-    'VorlaufHeizkreis',
-    'VorlaufWarmwasser',
-    'VorlaufSolar',
-    'VorlaufHolz',
-
-    'SpeicherMitte',
-    'SpeicherUnten',
-    'RuecklaufSolar',
-
-    'RuecklaufHolz',
 )
 
 
-_one_day_s = int(datetime.timedelta(days=1).total_seconds())
-_half_an_hour_s = int(datetime.timedelta(minutes=30).total_seconds())
+# move these to some utility module
+_one_hour = datetime.timedelta(hours=1)
+_one_minute = datetime.timedelta(minutes=1)
+_one_day_s = datetime.timedelta(days=1)
+_half_an_hour_s = datetime.timedelta(minutes=30)
 
 @app.flask.route('/')
 def home():
     return app.render_template(
         'faschingbauer_home.html',
-        thermometers = _thermometer_names,
-        duration = _one_day_s,
-        granularity = _half_an_hour_s,
+        thermometers = _top_thermometer_names,
+        now = datetime.datetime.now(),
+        duration = _one_hour,
+        granularity = _one_minute,
         svg = svg,
     )
