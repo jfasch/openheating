3fd31c0038831c1116858203f452063b81240306
Joerg Faschingbauer <jf@faschingbauer.co.at>
2020-01-28 10:13:58 +0100

    web: make switches/ a form with checkboxes

--- openheating/web/switches.py
+++ openheating/web/switches.py
@@ -1,8 +1,17 @@
 from .instance import app
 
+from flask import request
 
-@app.flask.route('/switches')
+
+@app.flask.route('/switches', methods=['GET', 'POST'])
 def switches():
+    if request.method == 'POST':
+        for name, switch in app.switches.items():
+            state = request.form.get('{}.state'.format(name))
+            if state is None:
+                state = False
+            switch.set_state(state)
+
     return app.render_template(
         'switches.html', 
     )
