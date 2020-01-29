85f765cdbfd8120e0f68c528be9dc2a2fb176468
Joerg Faschingbauer <jf@faschingbauer.co.at>
2020-01-29 15:56:57 +0100

    thermometers: don't overwrite existing simulation files

--- openheating/base/thermometer.py
+++ openheating/base/thermometer.py
@@ -1,6 +1,8 @@
 from .error import HeatingError
 
 from abc import ABCMeta, abstractmethod
+import os.path
+import logging
 
 
 class Thermometer(metaclass=ABCMeta):
@@ -15,11 +17,15 @@ class FileThermometer(Thermometer):
     def __init__(self, path, initial_value=None):
         self.__path = path
 
-        if initial_value is not None:
-            try:
-                with open(self.__path, 'x') as f:
-                    f.write(str(initial_value)+'\n')
-            except FileExistsError: pass
+        # file does not exist or is empty. write initial value.
+        if not os.path.isfile(self.__path) or os.path.getsize(self.__path) == 0:
+            with open(self.__path, 'w') as f:
+                if initial_value is None:
+                    raise HeatingError('{} does not exist and initial_value not given'.format(self.__path))
+                logging.debug('{}: initializing thermometer with {}'.format(self.__path, initial_value))
+                f.write(str(initial_value)+'\n')
+        elif initial_value is not None:
+            logging.debug('{}: already in place, not initializing (with {})'.format(self.__path, initial_value))
 
     def get_temperature(self):
         with open(self.__path) as f:
